8a10fb1eebf37a2dac32c3a7cc895e4258875cb7
Brian <brian@bteksoftware.com>
2021-12-11 17:01:55 -0700

    more tap keys

--- keyboards/keebio/iris/keymaps/sephory/keymap.c
+++ keyboards/keebio/iris/keymaps/sephory/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,           RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
+LSFT_T(KC_BSLS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,           RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LCTL, KC_LALT, LT(_LOWER, KC_SPC),        KC_ENT,  KC_BSPC,  KC_DEL
+                                    KC_LCTL, LALT_T(KC_F13), LT(_LOWER, KC_SPC),        KC_ENT,  KC_BSPC,  KC_DEL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
