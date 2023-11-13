4898837700a49377b31e9858a5d6ba77a86024e8
Brian Hill <brian@sephory.dev>
2023-11-13 13:14:19 -0700

    clean

--- keyboards/keebio/iris/keymaps/sephory/config.h
+++ keyboards/keebio/iris/keymaps/sephory/config.h
@@ -19,5 +19,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
-#define TAPPING_FORCE_HOLD
+#define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
--- keyboards/keebio/iris/keymaps/sephory/keymap.c
+++ keyboards/keebio/iris/keymaps/sephory/keymap.c
@@ -4,14 +4,17 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 3
+#define _GAME 3
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST
-};
+#define CTRL_ESC LCTL_T(KC_ESC)
+#define RAISE MO(_RAISE)
+#define RSFT_BSLS RSFT_T(KC_BSLS)
+#define LALT_F13 LALT_T(KC_F13)
+#define LOWER_SPC LT(_LOWER, KC_SPC)
+#define GAME TG(_GAME)
+#define DOT_ARROW LT(0, KC_DOT)
+
+static uint8_t scroll_horizontal = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -21,23 +24,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,         RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,           RAISE,  KC_N,    KC_M,    KC_COMM, DOT_ARROW,  KC_SLSH, RSFT_BSLS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LALT_T(KC_F13), LT(_LOWER, KC_SPC),        KC_ENT,  KC_BSPC,  KC_MINS
+                                    KC_LGUI, LALT_F13, LOWER_SPC,        KC_ENT,  KC_BSPC,  KC_MINS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______,  _______,                           KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
+     _______, _______, _______, _______, _______,  _______,                           KC_NUM, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, KC_LPRN, KC_RPRN,                            _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, KC_LCBR, KC_RCBR,                            _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,          TG(_ADJUST), _______, KC_P0,   KC_P0,   KC_PDOT, KC_PPLS, _______,
+     _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,          GAME,    _______, KC_P0,   KC_P0,   KC_PDOT, KC_PPLS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -57,7 +60,7 @@ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,         RAISE,  K
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_ADJUST] = LAYOUT(
+  [_GAME] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            RGB_TOG, RGB_MODE_FORWARD, RGB_MODE_REVERSE, RGB_SPI, RGB_SPD, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -65,7 +68,7 @@ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,         RAISE,  K
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, KC_M,             TG(_ADJUST), _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, KC_M,             GAME, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LCTL,  _______, KC_SPC,                    _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -74,61 +77,40 @@ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL,         RAISE,  K
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+    case LT(_LOWER, KC_SPC):
+        scroll_horizontal = record->event.pressed;
+        return true;
+    case DOT_ARROW:
+      if (record->tap.count == 0) {  // Key is being held.
+        if (record->event.pressed) {
+          SEND_STRING("->");
+        }
+        return false;  // Skip default handling.
       }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
       break;
   }
   return true;
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
     }
     else if (index == 1) {
         switch (biton32(layer_state)) {
-            case _LOWER:
-                clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
-                break;
             case _RAISE:
                 clockwise ? tap_code(KC_PGUP) : tap_code(KC_PGDN);
                 break;
-            case _ADJUST:
+            case _GAME:
                 clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
                 break;
             default:
-                clockwise ? tap_code(KC_UP) : tap_code(KC_DOWN);
+                if (scroll_horizontal) {
+                    clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
+                } else {
+                    clockwise ? tap_code(KC_UP) : tap_code(KC_DOWN);
+                }
         }
     }
+    return false;
 }
